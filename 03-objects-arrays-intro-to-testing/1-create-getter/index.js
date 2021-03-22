7031465683feec0d169a7c5f614191b6d14d1e97
alexbabich <ababich@splashworldwide.com>
2021-03-22 11:51:28 +0200

    03-objects-arrays-intro-to-testing: Fixed return / Deleted useless code

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -7,25 +7,15 @@ export function createGetter(path) {
   const fields = path.split('.');
 
   return function (product) {
-    let result;
+    let result = product;
 
     for (const field of fields) {
-      if (result) {
-        if (result.hasOwnProperty(field)) {
-          result = result[field];
-          continue;
-        }
-
-        return undefined;
-      }
-
-      if (product.hasOwnProperty(field)) {
-        result = product[field];
-
+      if (result.hasOwnProperty(field)) {
+        result = result[field];
         continue;
       }
 
-      return undefined;
+      return;
     }
 
     return result;
