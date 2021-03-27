50a064aff24c27b43fd1042746d58fa6e6649f6b
alexbabich <ababich@splashworldwide.com>
2021-03-27 14:59:48 +0200

    05-dom-document-loading: Solved first task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,72 @@
 export default class NotificationMessage {
+  static staticElement;
+  static setTimeout;
+  element;
 
+  constructor(
+    message = '',
+    {
+      duration = 0,
+      type = ''
+    } = {}
+  ) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.destroy();
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+          <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">
+                ${this.message}
+            </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    if (!NotificationMessage.staticElement) {
+      NotificationMessage.staticElement = element.firstElementChild;
+      this.element = NotificationMessage.staticElement;
+    }
+  }
+
+  show(targetElement) {
+    const body = document.querySelector('body');
+
+    if (targetElement) {
+      targetElement.innerHTML = this.element.outerHTML;
+    }
+
+    body.append(this.element);
+
+    this.remove();
+  }
+
+  destroy() {
+    if (NotificationMessage.staticElement) {
+      NotificationMessage.staticElement.remove();
+      NotificationMessage.staticElement = null;
+    }
+  }
+
+  remove() {
+    if (NotificationMessage.setTimeout) {
+      clearTimeout(NotificationMessage.setTimeout);
+    }
+
+    NotificationMessage.setTimeout = setTimeout(this.destroy, this.duration);
+  }
 }
